dba38e5bbd2d220061fde38d962b5687e43a91f0
Tawanda <tawandagdmavondo@gmail.com>
2021-08-12 15:52:28 +0200

    .

--- src/main/java/co/zw/gotour/server/GotourServerApplication.java
+++ src/main/java/co/zw/gotour/server/GotourServerApplication.java
@@ -2,7 +2,10 @@ package co.zw.gotour.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAdminServer
 public class GotourServerApplication {
 
     public static void main(String[] args) {
