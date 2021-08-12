bf35e971b2620be00e08cbf766b8d301d7ab5e54
Tawanda <tawandagdmavondo@gmail.com>
2021-08-13 00:20:49 +0200

    .

--- src/main/java/co/zw/gotour/server/Controller/UserController.java
+++ src/main/java/co/zw/gotour/server/Controller/UserController.java
@@ -1,11 +1,19 @@
 package co.zw.gotour.server.Controller;
 
+import co.zw.gotour.server.Configuration.LoggerConfig;
 import co.zw.gotour.server.Model.User;
 import co.zw.gotour.server.Service.UserService;
 import io.sentry.spring.tracing.SentrySpan;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,10 +32,21 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    private final Logger logger = LogManager.getLogger();
+
     @PostMapping()
     @SentrySpan
+    @ApiResponses(value = {
+
+            @ApiResponse(responseCode = "201", description = "Successfully Created User ", content = @Content(schema = @Schema(implementation = User.class))),
+
+            // TODO: Later insert the content of the Global Custon APIException Handler
+            // Response
+            @ApiResponse(responseCode = "400", description = "Username Already Exists")
+
+    })
     public ResponseEntity<?> createUser(@RequestBody User user) throws Exception {
-       
+
         try {
             var saved = userService.save(user);
             return ResponseEntity.status(HttpStatus.CREATED).body(saved);
