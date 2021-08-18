09d92b8ab7914ee30097c6c8d2b90b4e99715715
Tawanda <tawandagdmavondo@gmail.com>
2021-08-18 11:14:17 +0200

    Added Cors

--- src/main/java/co/zw/gotour/server/Controller/UserController.java
+++ src/main/java/co/zw/gotour/server/Controller/UserController.java
@@ -16,6 +16,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -29,6 +30,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User Endpoints")
 @RequestMapping(path = "/user")
 @RequiredArgsConstructor
+@CrossOrigin
+
 public class UserController {
 
     @Autowired
--- src/main/java/co/zw/gotour/server/GotourServerApplication.java
+++ src/main/java/co/zw/gotour/server/GotourServerApplication.java
@@ -3,10 +3,12 @@ package co.zw.gotour.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
+
 public class GotourServerApplication {
 
     public static void main(String[] args) {
